e822c04751248b8e4364bdd20e1668597cc036ea
mahdi salamat <fannimh36@gmail.com>
2021-07-30 21:43:03 -0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -2,81 +2,39 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className="w-full h-screen bg-myTheme">
+      <div className="w-full h-full  flex flex-row items-center" >
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className=" w-6/12 pl-60  flex justify-end items-center h-full">
+          <img src="/images/Phone.png" ></img>
         </div>
-      </main>
+        <div className="w-6/12 p-10 items-center  " >
+          <div className="flex flex-col bg-white w-72 p-2 items-center" >
+            <h1 className="mb-5" >Instagram</h1>
+            <input className="w-60 mb-5 bg-gray-300 p-1 rounded-md" type="text" placeholder="phone number, user name, or email" ></input>
+            <input className="w-60 mb-5 bg-gray-300 p-1 rounded-md" placeholder="password" ></input>
+            <button className="w-60 p-2 bg-blue-500 text-white rounded-md" >Log In</button>
+            <div className="m-1" >
+              or
+            </div>
+            <div className="m-2 text-blue-600" >Log in with facebook</div>
+            <div className="m-2" >forget password?</div>
+          </div>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+          <div className="bg-white mt-2 w-72 p-5 items-center" >Don't have an account?<a>Sign up</a></div>
+          <p className="m-2 pl-11" >Get the app</p>
+
+          <div className="flex" >
+            <img src="/images/Google-play.png" className="flex" ></img>
+            <img src="/images/App-store.png" className="flex" ></img>
+          </div>
+
+
+        </div>
+
+      </div>
+
+      <div></div>
     </div>
   )
 }
